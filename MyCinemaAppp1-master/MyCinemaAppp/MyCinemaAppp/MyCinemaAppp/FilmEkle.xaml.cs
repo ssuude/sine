b36618aa2de6f4bac70095fd123e48451f2b8231
ssuude <sdnr.elmas@hotmail.com>
2022-05-05 20:30:11 +0300

    blabla

---
+++ MyCinemaAppp1-master/MyCinemaAppp/MyCinemaAppp/MyCinemaAppp/FilmEkle.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyCinemaAppp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FilmEkle : ContentPage
+    {
+        public FilmEkle()
+        {
+            InitializeComponent();
+            txtGösterimTarih.Text = DateTime.Now.ToString();
+        }
+        private void eklebutonu(object sender, EventArgs e)
+        {
+            Film myMovie = new Film();
+            myMovie.MovieTitle = txtFilmAd.Text;
+            myMovie.Seats = new byte[] {10,20,30,40,50,60,70,80,90,100};
+            myMovie.Image = txtFilmResim.Text;
+            myMovie.ShowingDate = DateTime.Now.AddDays(15);
+
+            VeriTabani bag = new VeriTabani();
+            int sonuc = bag.Ekle(myMovie);
+            if (sonuc > 0)
+            {
+                DisplayAlert("ekleme durumu", "ekleme başarılı", "ok");
+            }
+            else
+            {
+                DisplayAlert("ekleme durumu", "başarısız", "ok");
+            }
+
+        }
+        private void filmListele(object sender, EventArgs e)
+        {
+            this.Navigation.PushModalAsync(new GirisPaneli());
+        }
+        private void filmGüncelle(object sender, EventArgs e)
+        {
+            this.Navigation.PushModalAsync(new GüncellemeSayfasi());
+        }
+    }
+}
